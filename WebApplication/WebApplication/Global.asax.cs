af314fc020bc03849483874f4c8d239c9d865661
enclaveTeam <vovansinh1991@gmail.com>
2015-07-01 13:39:23 +0700

    Test

--- WebApplication/WebApplication/Global.asax.cs
+++ WebApplication/WebApplication/Global.asax.cs
@@ -16,6 +16,8 @@ namespace WebApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
         }
     }
 }
