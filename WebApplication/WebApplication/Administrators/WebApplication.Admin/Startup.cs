be6550150f4727402fe2ed16b0eb8307980bd2ae
enclaveTeam <vovansinh1991@gmail.com>
2015-07-01 13:35:56 +0700

    Initial

---
+++ WebApplication/WebApplication/Administrators/WebApplication.Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication.Admin.Startup))]
+namespace WebApplication.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
