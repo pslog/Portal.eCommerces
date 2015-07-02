10eb291fa8df7818ecd1522b5ceaf456fb89fbe1
enclaveTeam <vovansinh1991@gmail.com>
2015-07-02 23:04:24 +0700

    add Product model

---
+++ WebApplication/WebApplication.Models/ViewModels/ProductPartialViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models.ViewModels
+{
+    public class ProductPartialViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Catagory { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
