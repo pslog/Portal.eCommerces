d6459859156d02698c1e414d83fb893ac5914b30
trungkienkaka <vovansinh1991@gmail.com>
2015-07-05 00:09:01 +0700

    update add and remove product from cart

--- WebApplication/WebApplication.Models/ViewModels/ProductPartialViewModel.cs
+++ WebApplication/WebApplication.Models/ViewModels/ProductPartialViewModel.cs
@@ -12,7 +12,8 @@ namespace WebApplication.Models.ViewModels
         public string Name { get; set; }
         public string Catagory { get; set; }
         public string Description { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public string Image { get; set; }
+        public int Quantity { get; set;}
     }
 }
--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.BusinessLogic.BusinessLogic;
 using WebApplication.Models.ViewModels;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        #region fields
+        private ProductBoard_Business _productBoard_Business = new ProductBoard_Business();
+        #endregion
         public ActionResult Index()
         {
             return View();
@@ -30,56 +34,7 @@ namespace WebApplication.Controllers
 
         public ActionResult ListProduct()
         {
-            List<ProductPartialViewModel> products = new List<ProductPartialViewModel>();
-            ProductPartialViewModel product1 = new ProductPartialViewModel(){
-                Id = 1,
-                Name = "Galaxy S5",
-                Catagory = "Samsung",
-                Description = "32GB, 2GB Ram, 1080HD, 5.1 inches, Android",
-                Price = 649.99,
-                Image = "http://placehold.it/650x450&text=Galaxy S5"
-            };
-             ProductPartialViewModel product2 = new ProductPartialViewModel(){
-                Id = 2,
-                Name = "iPhone 6",
-                Catagory = "Apple",
-                Description = "32GB, 64Bit, 1080HD, 4.7 inches, iOS 8",
-                Price = 749.99,
-                Image = "http://placehold.it/650x450&text=iPhone 6"
-            };
-             ProductPartialViewModel product3 = new ProductPartialViewModel(){
-                Id = 3,
-                Name = "Lumia 1520",
-                Catagory = "Nokia",
-                Description = "32GB, 4GB Ram, 1080HD, 6.3 inches, WP 8",
-                Price = 749.00,
-                Image = "http://placehold.it/650x450&text=Lumia 1520"
-            };
-             ProductPartialViewModel product4 = new ProductPartialViewModel()
-             {
-                 Id = 4,
-                 Name = "Lumia 1520",
-                 Catagory = "Nokia",
-                 Description = "32GB, 4GB Ram, 1080HD, 6.3 inches, WP 8",
-                 Price = 749.00,
-                 Image = "http://placehold.it/650x450&text=Lumia 1520"
-             };
-             ProductPartialViewModel product5 = new ProductPartialViewModel()
-             {
-                 Id = 5,
-                 Name = "Lumia 1520",
-                 Catagory = "Nokia",
-                 Description = "32GB, 4GB Ram, 1080HD, 6.3 inches, WP 8",
-                 Price = 749.00,
-                 Image = "http://placehold.it/650x450&text=Lumia 1520"
-             };
-            products.Add(product1);
-            products.Add(product2);
-            products.Add(product3);
-            products.Add(product4);
-            products.Add(product5);
-
-            return PartialView(products);
+            return PartialView( _productBoard_Business.GetAllProducts());
         }
     }
 }
